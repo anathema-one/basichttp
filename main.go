1cbf346a16f9f82eedb92cf62e91f709872af0f3
Greg Kalmring <gpkalm@gmail.com>
2019-11-27 10:45:43 -0500

    added seconadry route for 'foo'

--- main.go
+++ main.go
@@ -10,5 +10,9 @@ func main() {
 		fmt.Fprintf(w, "My Basic HTTP Server")
 	})
 
+	http.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "bar")
+	})
+
 	http.ListenAndServe(":80", nil)
 }
